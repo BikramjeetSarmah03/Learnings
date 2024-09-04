632134accb78cd26993acbd2d54df37b2942d40d
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2024-09-04 16:14:28 +0530

    register form

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -21,26 +21,27 @@ import { Button } from "@/components/ui/button";
 import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
 
-import { LoginSchema } from "@/schema";
-import { login } from "@/actions/login";
+import { RegisterSchema } from "@/schema";
+import { register } from "@/actions/register";
 
-interface LoginFormProps {}
+interface RegisterFormProps {}
 
-export function LoginForm({}: LoginFormProps) {
+export function RegisterForm({}: RegisterFormProps) {
   const [isPending, startTransition] = useTransition();
 
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
 
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
     defaultValues: {
       email: "",
       password: "",
+      name: "",
     },
   });
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
     /* CAN DO: can use api routes too
         axios.post("/api/login",values)
     */
@@ -49,7 +50,7 @@ export function LoginForm({}: LoginFormProps) {
     setSuccess("");
 
     startTransition(() => {
-      login(values).then((data) => {
+      register(values).then((data) => {
         setError(data.error);
         setSuccess(data.success);
       });
@@ -58,14 +59,32 @@ export function LoginForm({}: LoginFormProps) {
 
   return (
     <CardWrapper
-      headerLabel="Welcome Back"
-      backButtonLabel="Don't have an account ?"
-      backButtonHref="/auth/register"
+      headerLabel="Create an account"
+      backButtonLabel="Already have an account ?"
+      backButtonHref="/auth/login"
       showSocial
     >
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="Example"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
             <FormField
               control={form.control}
               name="email"
@@ -107,7 +126,7 @@ export function LoginForm({}: LoginFormProps) {
           <FormError message={error} />
           <FormSuccess message={success} />
           <Button type="submit" className="w-full" disabled={isPending}>
-            Login
+            Create an account
           </Button>
         </form>
       </Form>

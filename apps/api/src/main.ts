f584e05214de4b68a436c2a6373a1d209d50a056
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2024-09-18 19:34:39 +0530

    Common Libary

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { add } from '@learning/sample-lib';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log({ add: add(2, 3) });
+
   await app.listen(3000);
 }
 bootstrap();

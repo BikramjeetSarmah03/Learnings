bd23e46192285e08fcc0d1ef5f70007048da9760
Bikramjeet Sarmah <bikramjeet@boldtek.com>
2024-10-18 15:47:00 +0530

    drizzle zod for typesaftey

--- hono-node-prod-setup/src/routes/tasks/task.routes.ts
+++ hono-node-prod-setup/src/routes/tasks/task.routes.ts
@@ -3,6 +3,7 @@ import { createRoute, z } from "@hono/zod-openapi";
 import { jsonContent } from "stoker/openapi/helpers";
 
 import { HttpStatusCodes } from "@/lib/types";
+import { selectTasksSchema } from "@/db/schema";
 
 const tags = ["Tasks"];
 
@@ -12,12 +13,7 @@ export const list = createRoute({
   method: "get",
   responses: {
     [HttpStatusCodes.OK]: jsonContent(
-      z.array(
-        z.object({
-          name: z.string(),
-          done: z.boolean(),
-        })
-      ),
+      z.array(selectTasksSchema),
       "The list of tasks"
     ),
   },

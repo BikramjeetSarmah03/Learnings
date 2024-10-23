1b388cf0cc6e989fc1446d7471a0033a65261d7b
Bikramjeet Sarmah <bikramjeet@boldtek.com>
2024-10-23 12:53:11 +0530

    Auth Pages

---
+++ hono-auth-react/apps/nextjs/src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from "@/components/register-form";
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen px-4">
+      <RegisterForm />
+    </div>
+  );
+}

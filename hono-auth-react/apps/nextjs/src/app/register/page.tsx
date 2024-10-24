4f4ac562e17311d78e2d95f7e4102cfc9a0cad02
Bikramjeet Sarmah <bikramjeet@boldtek.com>
2024-10-24 10:12:50 +0530

    vite + tanstack + react

--- hono-auth-react/apps/nextjs/src/app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import { RegisterForm } from "@/components/register-form";
-
-export default function Page() {
-  return (
-    <div className="flex items-center justify-center w-full h-screen px-4">
-      <RegisterForm />
-    </div>
-  );
-}

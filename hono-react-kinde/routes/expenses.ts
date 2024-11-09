7e9880790c0f2846dc419f6fc13c09029e0796d5
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2024-11-09 22:58:08 +0530

    zod

--- hono-react-kinde/routes/expenses.ts
+++ hono-react-kinde/routes/expenses.ts
@@ -1,15 +1,45 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+
+import { createExpenseSchema } from "../shared/schemas";
+import { expenses } from "../shared/data/fake";
 
 export const expensesRoutes = new Hono()
   .get("/", (c) => {
     return c.json({
-      expenses: [],
+      expenses: expenses,
     });
   })
-  .post("/", (c) => {
+  .post("/", zValidator("json", createExpenseSchema), (c) => {
+    const data = c.req.valid("json");
+
+    expenses.push({ ...data, id: expenses.length + 1 });
+
     return c.json({
       expense: {},
     });
+  })
+  .get("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+
+    const expense = expenses.find((exp) => exp.id === id);
+
+    if (!expense) {
+      return c.notFound();
+    }
+
+    return c.json({ expense });
+  })
+  .delete("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+
+    const index = expenses.findIndex((exp) => exp.id === id);
+
+    if (index === -1) {
+      return c.notFound();
+    }
+
+    const deletedExpense = expenses.splice(index, 1)[0];
+
+    return c.json({ expense: deletedExpense });
   });
-//   .delete("/")
-//   .put("/");
---
+++ hono-react-kinde/shared/data/fake.ts
@@ -0,0 +1,24 @@
+import type { Expense } from "../schemas";
+
+export const expenses: Expense[] = [
+  {
+    id: 1,
+    title: "Expense 1",
+    amount: 1000,
+  },
+  {
+    id: 2,
+    title: "Expense 2",
+    amount: 2000,
+  },
+  {
+    id: 3,
+    title: "Expense 3",
+    amount: 3000,
+  },
+  {
+    id: 4,
+    title: "Expense 4",
+    amount: 4000,
+  },
+];

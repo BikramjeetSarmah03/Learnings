27e8ac710c1eeac95b3ed2e6a4190b12c11815c3
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2024-11-14 22:35:50 +0530

    build apps

--- hono-react-kinde/server/app.ts
+++ hono-react-kinde/server/app.ts
@@ -2,16 +2,16 @@ import { Hono } from "hono";
 
 import { expensesRoutes } from "./routes/expenses";
 import { logger } from "hono/logger";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 app.use("*", logger());
 
-app.get("/", (c) => {
-  return c.json({ message: "Hey working..." });
-});
-
 const apiRoutes = app.basePath("/api").route("/expenses", expensesRoutes);
+
+app.get("*", serveStatic({ root: "./client/dist" }));
+app.get("*", serveStatic({ path: "./client/dist/index.html" }));
 
 export default app;
 

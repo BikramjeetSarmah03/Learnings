f33a113e8aa91b7c0e92c22bf54344941e90c6aa
Bikramjeet Sarmah <bikramjeet@boldtek.com>
2024-11-15 11:12:41 +0530

    create expense form

--- hono-react-kinde/client/src/pages/create-expense.tsx
+++ hono-react-kinde/client/src/pages/create-expense.tsx
@@ -1,9 +1,25 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
 export const Route = createFileRoute("/create-expense")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return "Hello /create-expense!";
+  return (
+    <div className="p-2">
+      <form className="max-w-4xl p-4 m-auto">
+        <Label htmlFor="title">Title</Label>
+        <Input type="text" id="title" placeholder="Title" />
+
+        <Label htmlFor="amont">Amount</Label>
+        <Input type="number" id="amont" placeholder="Amount" />
+
+        <Button type="submit">Save</Button>
+      </form>
+    </div>
+  );
 }

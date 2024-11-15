5ae88555b76a2e8ca54faf07c16abb36dae1e3fa
Bikramjeet Sarmah <bikramjeet@boldtek.com>
2024-11-15 10:50:24 +0530

    get all expenses

--- hono-react-kinde/client/src/pages/expenses.tsx
+++ hono-react-kinde/client/src/pages/expenses.tsx
@@ -1,9 +1,80 @@
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
+import { apiClient } from "@/lib/api";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Skeleton } from "@/components/ui/skeleton";
+
+async function getAllExpenses() {
+  const res = await apiClient.expenses.$get();
+
+  if (!res.ok) throw new Error("Server error");
+
+  const data = await res.json();
+  return data;
+}
+
 export const Route = createFileRoute("/expenses")({
-  component: RouteComponent,
+  component: Expenses,
 });
 
-function RouteComponent() {
-  return "Hello /expenses!";
+function Expenses() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["all-expennses"],
+    queryFn: getAllExpenses,
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <div className="max-w-4xl p-2 m-auto">
+      <Table>
+        <TableCaption>A list of your expenses.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Expense</TableHead>
+            <TableHead>Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {isPending
+            ? Array(3)
+                .fill(0)
+                .map((_, i) => (
+                  <TableRow key={i}>
+                    <TableCell>
+                      <Skeleton className="h-4" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-4" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-4" />
+                    </TableCell>
+                  </TableRow>
+                ))
+            : data.expenses.map((expense) => (
+                <TableRow key={expense.id}>
+                  <TableCell className="font-medium">{expense.id}</TableCell>
+                  <TableCell>{expense.title}</TableCell>
+                  <TableCell>{expense.amount}</TableCell>
+                </TableRow>
+              ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }

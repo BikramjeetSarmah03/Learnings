7fc57763b2fddf9e6c01417d27523ffc75843afe
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2024-12-19 00:35:53 +0530

    Revert "shadcn"
    This reverts commit 003f1e6f4cb3ce2509c9407cfdd46cf522c9de86.

--- svelte-crud/src/routes/+page.svelte
+++ svelte-crud/src/routes/+page.svelte
@@ -1,8 +1,2 @@
-<script lang="ts">
-    import {Button} from "@/components/ui/button"
-</script>
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<Button>Hey</Button>

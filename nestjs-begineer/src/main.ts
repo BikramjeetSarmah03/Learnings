0b2b10d9b2dab79856ebad94a801c531baa5a2c4
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2025-01-18 00:39:53 +0530

    Scalar Docs

--- nestjs-begineer/src/main.ts
+++ nestjs-begineer/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Docs')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('Index')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+
+  app.use(
+    '/api/docs',
+    apiReference({
+      spec: {
+        content: documentFactory,
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();

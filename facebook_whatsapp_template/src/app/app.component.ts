59e0191d9668e97ffabe5426c206971e2df4fa8c
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2025-03-01 01:12:02 +0530

    Template Form Base

--- facebook_whatsapp_template/src/app/app.component.ts
+++ facebook_whatsapp_template/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TemplatePreviewComponent } from './components/template-preview/template-preview.component';
+import { TemplateFormComponent } from './components/template-form/template-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TemplatePreviewComponent],
+  imports: [TemplatePreviewComponent, TemplateFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

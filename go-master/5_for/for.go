767c206e9cc14fe43db8e27cfb3d022ba7ba23d4
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2025-05-14 23:09:55 +0530

    for loop

---
+++ go-master/5_for/for.go
@@ -0,0 +1,30 @@
+package main
+
+// for -> only construct in go for looping
+func main() {
+
+	// while loop
+	// i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	// }
+
+	// infinite loop
+	// for {
+	// 	fmt.Println("1")
+	// }
+
+	// classic for loop
+	// for i := 0; i < 3; i++ {
+	//  break
+	//  continue
+	// 	fmt.Println(i)
+	// }
+
+	// 1.22 range
+	// for i := range 3 {
+	// 	fmt.Println(i)
+	// }
+
+}

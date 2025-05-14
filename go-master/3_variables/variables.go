99303b24202749eb967f67a455ffa3824ca19025
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2025-05-14 21:56:28 +0530

    variables

---
+++ go-master/3_variables/variables.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+
+	// var name string = "golang" // with type ( explict )
+	// var name = "golang" // without type ( infer )
+
+	// var isAdult bool = true
+
+	// var age int = 30
+
+	// shorthand syntax
+	// name := "golang" // commonly used when we are making and assigning on same line
+
+	// var name string // defining later
+	// name = "golang"
+
+	// var price float32 = 50.5
+	// var price = 50.5
+	// price := 50.5
+
+	// fmt.Println(price)
+}

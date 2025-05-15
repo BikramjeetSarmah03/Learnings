1ad6c73d222b4b35e7b5526cd49a41f24c8fb3e1
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2025-05-15 08:56:05 +0530

    arrays

---
+++ go-master/8_array/arrays.go
@@ -0,0 +1,33 @@
+package main
+
+// numbered sequence of specific length
+func main() {
+
+	// zero values in int array
+	// var nums [10]int
+
+	// nums[0] = 1
+
+	// fmt.Println(nums[0])
+	// fmt.Println(nums)
+	// array length
+	// fmt.Println(len(nums))
+
+	// default false values in bool array
+	// var vals [2]bool
+
+	// default empty string in string array
+	// var name [3]string
+
+	// to declare in single line
+	// nums := [3]int{1, 2, 3}
+	// fmt.Println(nums)
+
+	// 2d arrays
+	// nums := [2][2]int{{2, 4}, {5, 3}}
+	// fmt.Println(nums)
+
+	// - Fixed size, that is predectiable
+	// - Memory optimization
+	// - Constant Time Access
+}
